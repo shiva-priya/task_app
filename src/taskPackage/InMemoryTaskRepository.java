4dc809bb165129b7448f8f6cbf9ee0b93f349b35
priyad <shiva.priya@pramati.com>
2019-12-24 17:32:00 +0530

    Added In Memory Task Repsitory

---
+++ src/taskPackage/InMemoryTaskRepository.java
@@ -0,0 +1,63 @@
+package taskPackage;
+
+import java.time.LocalDate;
+import java.util.Iterator;
+import java.util.LinkedList;
+import java.util.List;
+
+public class InMemoryTaskRepository {
+
+    List<Task> taskData = new LinkedList<>();
+
+    public void addTask(String taskName, String description, LocalDate dueDate, TaskStatus status, int id) {
+        taskData.add(new Task(taskName, description, dueDate, status, id));
+    }
+
+    public List<Task> listTasks() {
+        return taskData;
+    }
+
+    public Task searchTask(String key) {
+        Task object = null;
+        for (Task t : taskData) {
+            if (t.getTaskName().equals(key)) {
+                object = t;
+            }
+        }
+        return object;
+    }
+
+    public Task searchByTaskID(int key) {
+        Task object = null;
+        for (Task t : taskData) {
+            if (t.getId() == key) {
+                object = t;
+            }
+        }
+        return object;
+    }
+
+    public List<Task> listByStatus(String status) {
+        List<Task> statusList = new LinkedList<>();
+        for (Task t : taskData) {
+            if (t.getStatus().toString().equals(status)) {
+                statusList.add(t);
+            }
+        }
+        return statusList;
+    }
+
+    public int deleteTask(String name) {
+        int flag = 0;
+        Iterator<Task> dataIterator = taskData.iterator();
+        while (dataIterator.hasNext()) {
+            Task obj = dataIterator.next();
+            String tname = obj.getTaskName();
+            if (name.equals(tname)) {
+                taskData.remove(obj);
+                flag = 1;
+            }
+        }
+        return flag;
+    }
+}
--- src/taskPackage/TaskManager.java
+++ src/taskPackage/TaskManager.java
@@ -7,59 +7,31 @@ import java.util.List;
 
 public class TaskManager {
 
-    List<Task> taskData = new LinkedList<>();
+   InMemoryTaskRepository inMemory = new InMemoryTaskRepository();
     int taskCount = 0;
 
     public void addTask(String taskName, String description, LocalDate dueDate, TaskStatus status) {
-        taskData.add(new Task(taskName, description, dueDate, status, ++taskCount));
+        inMemory.addTask(taskName, description, dueDate, status, ++taskCount);
     }
 
     public List<Task> listTasks() {
-        return taskData;
+        return inMemory.listTasks();
     }
 
     public Task searchTask(String key) {
-        Task object = null;
-        for (Task t : taskData) {
-            if (t.getTaskName().equals(key)) {
-                object = t;
-            }
-        }
-        return object;
+       return inMemory.searchTask(key);
     }
 
     public Task searchByTaskID(int key) {
-        Task object = null;
-        for (Task t : taskData) {
-            if (t.getId() == key) {
-                object = t;
-            }
-        }
-        return object;
+        return inMemory.searchByTaskID(key);
     }
 
     public List<Task> listByStatus(String status) {
-        List<Task> statusList = new LinkedList<>();
-        for (Task t : taskData) {
-            if (t.getStatus().toString().equals(status)) {
-                statusList.add(t);
-            }
-        }
-        return statusList;
+        return inMemory.listByStatus(status);
     }
 
     public int deleteTask(String name) {
-        int flag = 0;
-        Iterator<Task> dataIterator = taskData.iterator();
-        while (dataIterator.hasNext()) {
-            Task obj = dataIterator.next();
-            String tname = obj.getTaskName();
-            if (name.equals(tname)) {
-                taskData.remove(obj);
-                flag = 1;
-            }
-        }
-        return flag;
+      return inMemory.deleteTask(name);
     }
 
     public int getTaskCount() {
